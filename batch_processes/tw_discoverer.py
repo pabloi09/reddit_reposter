a311540661247d055504eaa6bf2314e77aba588e
Pablo <pablo.arribas100@gmail.com>
2021-01-06 18:03:26 +0100

    Logic changes to the twitter engagement process

--- batch_processes/tw_discoverer.py
+++ batch_processes/tw_discoverer.py
@@ -4,13 +4,13 @@ from twitter_util.TwitterUtil import TwitterUtil
 dbAPI = Database("db.db")
 
 for project in dbAPI.get_projects():
-    tw_util = TwitterUtil(project.twitter_config)
-    eng = dbAPI.get_next_tw_engagement_account(project.project_id)
-
-    try:
-        result = tw_util.get_followers_of(eng.username, eng.cursor)
-        eng.cursor = result["cursor"]
-        dbAPI.update_tw_cursor(eng)
-        dbAPI.add_tw_followers(result["followers"], eng.eng_id)
-    except Exception as e:
-        print(e)
+    if dbAPI.no_more_tw_accounts_to_follow(project.project_id):
+        try:
+            eng = dbAPI.get_next_tw_engagement_account(project.project_id)
+            tw_util = TwitterUtil(project.twitter_config)
+            result = tw_util.get_followers_of(eng.username, eng.cursor)
+            eng.cursor = result["cursor"]
+            dbAPI.update_tw_cursor(eng)
+            dbAPI.add_tw_followers(result["followers"], eng.eng_id)
+        except Exception as e:
+            print(e)
--- batch_processes/tw_unfollower.py
+++ batch_processes/tw_unfollower.py
@@ -5,11 +5,13 @@ from twitter_util.TwitterUtil import TwitterUtil
 dbAPI = Database("db.db")
 
 for project in dbAPI.get_projects():
-    
+    account = None
     try:
-        account = dbAPI.get_next_tw_user_to_follow(project.project_id)
+        account = dbAPI.get_next_tw_user_to_unfollow(project.project_id)
         tw_util = TwitterUtil(project.twitter_config)
-        tw_util.follow(account.user_id)
-        dbAPI.record_tw_follow(account)
+        tw_util.unfollow(account.user_id)
     except Exception as e:
         print(e)
+    finally:
+        if account is not None:
+            dbAPI.record_tw_unfollow(account)
