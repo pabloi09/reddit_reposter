f4e6b686a72fdc7fda6284942772ddde75300fca
Pablo <pablo.arribas100@gmail.com>
2021-01-15 13:38:27 +0100

    Correcting stop script bug

--- stop.sh
+++ stop.sh
@@ -1,7 +1,7 @@
 #bin/bash
 docker exec -it reposter_app sqlite3 db.db "update post set closed=1;"
-docker cp reposter_app:/reposter/db.db .
-docker cp reposter_app:/reposter/reposter.key .
+docker cp reposter_app:/reposter/db.db ./reposter/
+docker cp reposter_app:/reposter/reposter.key ./reposter/
 docker rm -f reposter_app
 docker system prune -f
 docker volume prune -f
