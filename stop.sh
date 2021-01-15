fb855e1b60c79578875c866b9fba225eb21b32f7
Pablo <pablo.arribas100@gmail.com>
2021-01-15 12:26:14 +0100

    New script for stopping container

---
+++ stop.sh
@@ -0,0 +1,6 @@
+#bin/bash
+docker exec -it reposter_app sqlite3 db.db "update post set closed=1;"
+docker cp reposter_app:/reposter/db.db /reposter
+docker rm -f reposter_app
+docker system prune -f
+docker volume prune -f
